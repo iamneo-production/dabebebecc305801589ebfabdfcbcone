59e5b7f12674b627ac44fabf28bc38110094da64
Iamneo <techadmin@iamneo.ai>
2023-10-05 13:23:13 +0000

    Backup Commit

--- dotnetproject/dotnetapigateway/Program.cs
+++ dotnetproject/dotnetapigateway/Program.cs
@@ -1,6 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Configuration.AddJsonFile("Ocelot.json",optional:false,reloadOnChange:true);
+builder.Services.AddOcelot(builder.Configuration);
+
 app.MapGet("/", () => "Hello World!");
 
+
+app.MapControllers();
+await app.UseOcelot();
 app.Run();
