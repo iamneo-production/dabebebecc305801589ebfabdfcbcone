9a5112ed94fe57d1c1158d741348f928792b524d
Iamneo <techadmin@iamneo.ai>
2023-10-05 12:37:36 +0000

    Backup Commit

--- dotnetproject/dotnetmicroservicetwo/Models/Review.cs
+++ dotnetproject/dotnetmicroservicetwo/Models/Review.cs
@@ -4,5 +4,9 @@ using System.Collections.Generic;
 namespace dotnetmicroservicetwo.Models;
 public class Review
 {
-
+    public int ReviewID{get;set;}
+    public string ArticleID{get;set;}
+    public string ReviewerName{get;set;}
+    public string Comments{get;set;}
+    public int Rating{get;set;}
 }
--- dotnetproject/dotnetmicroservicetwo/Models/ReviewDbContext.cs
+++ dotnetproject/dotnetmicroservicetwo/Models/ReviewDbContext.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using dotnetmicroservicestwo.
 namespace dotnetmicroservicetwo.Models;
 
 public class ReviewDbContext : DbContext
